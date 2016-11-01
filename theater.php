1fc7a32a40fe4ff863fa6e3c358e29ce639947df
copeck0399 <koganeicity@hotmail.co.jp>
2016-11-01 12:18:57 +0900

    create pages

---
+++ theater.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+	<head>
+		<meta charset="utf-8">
+		<title>top</title>
+		<link rel="stylesheet" href="css/headfooter.css">
+	</head>
+	<body>
+<?php
+	//ヘッダーの読み込み
+	require_once( "header.html" );
+?>
+<?php
+	//フッターの読み込み
+	require_once( "footer.html" );
+?>
+	</body>
+</html>
